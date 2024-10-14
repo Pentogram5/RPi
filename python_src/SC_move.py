7d9b9e9e46b26d220822ca6bc20360aa5cde88d3
Fe0dor MacBook Air M1 <woolpig@yandex.ru>
2024-10-14 12:39:31 +0500

    add SC_move

---
+++ python_src/SC_move.py
@@ -0,0 +1,35 @@
+import os
+import time
+
+from xr_motor import RobotDirection
+go = RobotDirection()
+
+def setMotor(motor, direction, speed):
+    go.set_speed(motor, speed * abs(direction - 1))
+    if direction <= 1:
+        if motor == 1:
+            go.m1m2_forward()
+        else:
+            go.m3m4_forward()
+    else:
+        if motor == 1:
+            go.m1m2_reverse()
+        else:
+            go.m3m4_reverse()
+
+
+def setDirection(direction, speed):
+    if direction == 0.0:
+        setMotor(1, 0, speed)
+        setMotor(2, 0, speed)
+    elif direction > 0:
+        setMotor(1, abs(direction), speed)
+        setMotor(2, 0, speed)
+    else:
+        setMotor(1, 0, speed)
+        setMotor(2, abs(direction), speed)
+
+while True:
+    direction = float(input())
+    speed = 50
+    setDirection(direction, speed)
