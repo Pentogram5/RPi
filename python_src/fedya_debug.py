d3eb5b19fe9033f3cbc5ffe3eece0e8c0e3c9fb1
Fe0dor MacBook Air M1 <woolpig@yandex.ru>
2024-10-14 16:55:45 +0500

    2

--- python_src/fedya_debug.py
+++ python_src/fedya_debug.py
@@ -2,7 +2,7 @@ from SC_infrared import *
 import time
 
 while True:
-    print(RI_1.getSerialise(), RI_1.getNewValue())
-    print(RI_2.getSerialise(), RI_1.getNewValue())
-    print(RI_3.getSerialise(), RI_1.getNewValue())
+    print(IR_1.getSerialise(), IR_1.getNewValue())
+    print(IR_2.getSerialise(), IR_1.getNewValue())
+    print(IR_3.getSerialise(), IR_1.getNewValue())
     time.sleep(0.1)
