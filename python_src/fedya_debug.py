98b126f5d715dd70326ea37662994f9f7dcc6d5d
Fe0dor MacBook Air M1 <woolpig@yandex.ru>
2024-10-15 20:52:23 +0500

    3

--- python_src/fedya_debug.py
+++ python_src/fedya_debug.py
@@ -4,6 +4,7 @@ servo.set(1, 90)
 
 S = ScServo()
 
+'''
 while True:
     d, servo1, t, angle = map(float, input().split())
     d = int(d)
@@ -19,3 +20,25 @@ while True:
     print(S.currentState)
     print(time.time() - begintime)
 
+'''
+
+while True:
+    inp = input()
+    if inp == 'c':
+        S.executeTrajectory(S.calcTrajectory(S.catchState))
+    elif inp == 'e':
+        S.executeTrajectory(S.calcTrajectory(S.expeditionState))
+    elif inp == 'set c':
+        while True:
+            n, t = int(input()), float(input())
+            if n == 0:
+                break
+            S.catchState[n]['time'] = t
+    elif inp == 'set e':
+        while True:
+            n, t = int(input()), float(input())
+            if n == 0:
+                break
+            S.expeditionState[n]['time'] = t
+    else:
+        break
