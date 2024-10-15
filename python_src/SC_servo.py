a0109225c6410d7d5b6d808df48fe6958e09b5d8
Fe0dor MacBook Air M1 <woolpig@yandex.ru>
2024-10-15 16:05:12 +0500

    3

--- python_src/SC_servo.py
+++ python_src/SC_servo.py
@@ -1,8 +1,51 @@
 from xr_servo import Servo
 servo = Servo()
 
+import numpy as np
+import time
+
+
 class ScServo:
     samplingRate = 100
-    def smoothlyMove(self, time, startAngle, stopAngle):
-        return list(range(startAngle, stopAngle, self.samplingRate * time))
-    def transportMode(self):
+    currentState = {1: 90,
+                    2: 90,
+                    3: 90,
+                    4: 90,
+                    5: 90,
+                    6: 90
+                    }
+    expeditionState = {1: {'time': 1, 'stopAngle': 10}}
+
+    # Рассчёт плавной траектории. Время траветории в секундах, начальный угол, конечный угол
+    def calcSmothMove(self, time, startAngle, stopAngle):
+        t = np.linspace(0, 1, self.samplingRate * time)
+        bezier = t * t * (3 - 2 * t)
+        angles = bezier * abs(stopAngle - startAngle)
+        angles += startAngle if startAngle < stopAngle else stopAngle
+        if not startAngle < stopAngle:
+            angles = angles[::-1]
+        angles = np.rint(angles).astype(int)
+        return angles
+    
+    def calcTrajectory(self, endState):
+        trajectory = endState.copy()
+        for key in endState.keys():
+            trajectory[key] = self.calcSmothMove(self.expeditionState[key]['time'],
+                                                 self.currentState[key],
+                                                 self.expeditionState[key]['stopAngle'])
+    
+    def executeTrajectory(self, trajectory):
+        count = 0
+        while True:
+            for key in trajectory.keys():
+                if trajectory[key] != None:
+                    try:
+                        angle = trajectory[key][count]
+                        servo.set(key, angle)
+                    except:
+                        trajectory[key] = None
+                    count += 1
+                    break
+            else:
+                break
+            time.sleep(1 / self.samplingRate)
--- python_src/fedya_debug.py
+++ python_src/fedya_debug.py
@@ -1,8 +1,11 @@
-from SC_infrared import *
+from SC_servo import *
 import time
 
+S = ScServo()
+
 while True:
-    print(IR_1.getSerialise(), IR_1.getNewValue())
-    print(IR_2.getSerialise(), IR_1.getNewValue())
-    print(IR_3.getSerialise(), IR_1.getNewValue())
-    time.sleep(0.1)
+    servo, t, angle = map(int, input().split())
+    state = {servo: {'time': t, 'stopAngle': angle}}
+    T = S.calcTrajectory(state)
+    print(T)
+    
