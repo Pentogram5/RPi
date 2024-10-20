84161a226ae8536e0fec4dd6eb0e6a00c837ac57
Leha <flex@gmail.com>
2024-10-20 15:47:08 +0500

    add fix control led

---
+++ python_src/control_led.py
@@ -0,0 +1,16 @@
+from xr_car_light import Car_light
+
+import time
+
+def red_light(Car_light):
+    Car_light.set_ledgroup(1, 8, 1)
+    
+def green_light(Car_light):
+    Car_light.set_ledgroup(1, 8, 4)
+    
+cl = Car_light()
+while True:
+    red_light(cl)
+    time.sleep(2)
+    green_light(cl)
+    time.sleep(2)
